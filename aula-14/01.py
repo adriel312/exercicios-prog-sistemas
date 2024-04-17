dd949126b7a4df1143fe7a1d6ae7ce59c96a5d33
adriel312 <adriel.cfa@gmail.com>
2024-04-16 22:04:03 -0300

    Exercícios Resolvidos

---
+++ aula-14/01.py
@@ -0,0 +1,11 @@
+nomes =["Jack","Kate","Sawyer","Charlie"]
+nomes.append("Jubiscreiton")
+nomes.append("Jubiscreiton")
+
+nomes.insert(2,"Cleitin do Brega")
+
+#nomes.remove("Jubiscreiton")
+#nome_pop = nomes.pop(0)
+print(nomes)
+#print(nomes[5].count("i"))
+print(len(nomes))
---
+++ aula-15/02.py
@@ -0,0 +1,8 @@
+matriz = [[0,0],
+          [0,0]]
+
+for i in range(2):
+    for j in range(2):
+        matriz[i][j] = int(input("Informe o valor: "))
+
+print(matriz)
